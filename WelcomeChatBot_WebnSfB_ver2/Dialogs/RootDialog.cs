7bc0a72b7219cd1252683ff720038169517ae651
Takunori <t-tamina@microsoft.com>
2018-10-29 16:17:42 +0900

    Metadata included

--- WelcomeChatBot_WebnSfB_ver2/Dialogs/RootDialog.cs
+++ WelcomeChatBot_WebnSfB_ver2/Dialogs/RootDialog.cs
@@ -5,7 +5,9 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using WelcomeChatBot_WebnSfB_ver2.Models;
+using System.Linq;
 
 namespace WelcomeChatBot_WebnSfB_ver2.Dialogs
 {
@@ -63,13 +65,25 @@ namespace WelcomeChatBot_WebnSfB_ver2.Dialogs
             int i;
             string resultMessage = "以下から選択してください(番号で入力)\n";
 
+            JObject jsonObject1 = JObject.Parse(json);
+
             if (flag == 0)
             {
                 if (result.Answers.Count >= displayNum)
                 {
                     for (i = 0; i < displayNum; i++)
                     {
-                        resultMessage = resultMessage + (i + 1).ToString() + ". " + result.Answers[i].Questions[0] + "\n";
+                        JObject jsonObject2 = JObject.Parse(jsonObject1["answers"][i].ToString());
+
+                        if (jsonObject2.Last.Last.Count() == 0)
+                        {
+                            resultMessage = resultMessage + (i + 1).ToString() + ". " + result.Answers[i].Questions[0] + "\n";
+                        }
+                        else
+                        {
+                            resultMessage = resultMessage + (i + 1).ToString() + ". " + "[" + (string)httpResponseJson.answers[i].metadata[0].value + "]" + result.Answers[i].Questions[0] + "\n";
+                        }
+                        
                     }
 
                     resultMessage = resultMessage + (i + 1).ToString() + ". 次の候補を表示する\n";
@@ -82,7 +96,16 @@ namespace WelcomeChatBot_WebnSfB_ver2.Dialogs
                 {
                     for (i = 0; i < result.Answers.Count; i++)
                     {
-                        resultMessage = resultMessage + (i + 1).ToString() + ". " + result.Answers[i].Questions[0] + "\n";
+                        JObject jsonObject2 = JObject.Parse(jsonObject1["answers"][i].ToString());
+
+                        if(jsonObject2.Last.Last.Count() == 0)
+                        {
+                            resultMessage = resultMessage + (i + 1).ToString() + ". " + result.Answers[i].Questions[0] + "\n";
+                        }
+                        else
+                        {
+                            resultMessage = resultMessage + (i + 1).ToString() + ". " + "[" + (string)httpResponseJson.answers[i].metadata[0].value + "]" + result.Answers[i].Questions[0] + "\n";
+                        }            
                     }
 
                     resultMessage = resultMessage + (i + 1).ToString() + ". 上記のどれでもない\n";
@@ -99,7 +122,16 @@ namespace WelcomeChatBot_WebnSfB_ver2.Dialogs
             {
                 for (i = displayNum; i < result.Answers.Count; i++)
                 {
-                    resultMessage = resultMessage + (i + 1).ToString() + ". " + result.Answers[i].Questions[0] + "\n";
+                    JObject jsonObject2 = JObject.Parse(jsonObject1["answers"][i].ToString());
+
+                    if (jsonObject2.Last.Last.Count() == 0)
+                    {
+                        resultMessage = resultMessage + (i + 1).ToString() + ". " + result.Answers[i].Questions[0] + "\n";
+                    }
+                    else
+                    {
+                        resultMessage = resultMessage + (i + 1).ToString() + ". " + "[" + (string)httpResponseJson.answers[i].metadata[0].value + "]" + result.Answers[i].Questions[0] + "\n";
+                    }
                 }
 
                 resultMessage = resultMessage + (i + 1).ToString() + ". 上記のどれでもない\n";
